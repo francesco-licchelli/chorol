01a3cf4ff3c15932d535944d73f5f57000fb7a58
Francesco Licchelli <francesco.licchelli@studio.unibo.it>
2025-02-26 23:13:21 +0100

    Fine commit precedente
    Signed-off-by: Francesco Licchelli <francesco.licchelli@studio.unibo.it>

--- src/main/java/it/unibo/tesi/chorol/utils/OutputSettings.java
+++ src/main/java/it/unibo/tesi/chorol/utils/OutputSettings.java
@@ -2,6 +2,7 @@ package it.unibo.tesi.chorol.utils;
 
 public class OutputSettings {
 	private static boolean fullType = false;
+	private static boolean saveStdLib = false;
 
 	public static boolean getFullType() {
 		return OutputSettings.fullType;
@@ -9,5 +10,13 @@ public class OutputSettings {
 
 	public static void setFullType(boolean fullType) {
 		OutputSettings.fullType = fullType;
+	}
+
+	public static void setSaveStdLib(boolean saveStdLib) {
+		OutputSettings.saveStdLib = saveStdLib;
+	}
+
+	public static boolean shouldSaveStdLib() {
+		return OutputSettings.saveStdLib;
 	}
 }
